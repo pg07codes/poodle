e9a0bcadc70660346e735fe5361ecb7a7587c3d2
pg07codes <pg07codes@gmail.com>
2020-05-06 11:44:08 +0530

    structure setup

---
+++ src/components/Search/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+
+export function Search(){
+    return (
+        <TextField id="standard-basic" label="Standard" />
+    );
+}
